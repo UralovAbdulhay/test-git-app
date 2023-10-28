7e06074e873c0a57662605b3e115fcbb0e3953f1
bekmurod28 <bekmurodurinbekov81@gmail.com>
2023-10-28 18:51:46 +0500

    add2

--- src/main/java/com/example/controller/CountController.java
+++ src/main/java/com/example/controller/CountController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CountController {
     private final CounterRepository repository;
     //there
+    //hill
 
     public CountController(CounterRepository repository) {
         this.repository = repository;
